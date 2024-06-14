5114edd8ed5a39f283904970ff18fb55eb862223
Firas <firasdabbabi@gmail.com>
2024-06-14 14:03:41 +0100

    a.

--- src/components/verify/CompleteVerify.tsx
+++ src/components/verify/CompleteVerify.tsx
@@ -1,11 +1,19 @@
-export default function CompleteVerify({userId, secret}){
+type CompleteVerifyTypes = {
+    userId: string;
+    secret: string;
+}
+
+export default function CompleteVerify({userId, secret}: CompleteVerifyTypes){
 
     // Update the page title
     document.title = `Glori | Complete Verification`;
 
+    const uid = userId;
+    const sec = secret;
+
     return (
         <>
-        Complete Verify Content
+        Complete Verify Content: {uid} , {sec}
         </>
     )
 }
