45f3db4e961d5531ecb3e0770f792664ad39dac7
Firas <firasdabbabi@gmail.com>
2024-07-25 20:36:12 +0100

    a.

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -399,7 +399,7 @@ export default function Navbar() {
                                     {/* Search section */}
                                     <Popover>
                                         <PopoverTrigger asChild>
-                                            <Button variant="outline" className="text-slate-500 border-0 shadow-none pr-5 bg-transparent">
+                                            <Button variant="outline" className="hidden text-slate-500 border-0 shadow-none pr-5 bg-transparent">
                                                 <CiSearch size="20px" className="mr-2" />
                                                 Find Perfumes
                                             </Button>
