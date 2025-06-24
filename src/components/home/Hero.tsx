78fd62c0fab84e8612a403163fc537d7710e5397
Firas <97522852+fifolio@users.noreply.github.com>
2025-06-24 16:56:27 +0100

    Update Hero.tsx

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -25,23 +25,26 @@ export default function Hero() {
   const season = getSeason(currentMonth);
 
   return (
-    <section className="py-12 md:py-24 lg:py-12">
-      <div className="grid gap-6 md:gap-8 px-4 md:px-6">
-        <div className="space-y-3">
-          <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
-            Welcome to Our {season} Collection
-          </h1>
-          <p className="max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-            Discover our latest selection of cozy and stylish {season}-inspired Perfumes.
-          </p>
-        </div>
+  <section className="py-12 md:py-24 lg:py-12">
+  <div className="grid gap-6 md:gap-8 px-4 md:px-6">
+    <div className="space-y-3">
+      <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+        Welcome to Our {season} Collection
+      </h1>
+      <p className="max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
+        Discover our latest selection of cozy and stylish {season}-inspired Perfumes.
+      </p>
+    </div>
 
-        <Carousel className="w-full hover:cursor-w-resize">
-          <CarouselContent>
-            <Skeleton className={`md:h-[400px] h-[155px] w-full rounded-xl ${skeleton ? '' : 'hidden'}`} />
-          </CarouselContent>
-        </Carousel>
-      </div>
-    </section>
+    <div className="w-full shadow-md">
+      <img
+        src="https://amiroud.com/media/wysiwyg/homepage/2023-current/1-perfume.jpg"
+        alt={`${season} collection perfume`}
+        className="w-full md:h-[400px] h-[155px] rounded-xl object-cover"
+      />
+    </div>
+  </div>
+</section>
+
   );
 }
